4d091cbe1ab7ae20bd98450f8b8a1d8e68ed0dce
unknown <psnforhoman@gmail.com>
2025-04-24 19:50:05 +0330

    button added for gaming master

--- src/components/MasterArtGaming.jsx
+++ src/components/MasterArtGaming.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber';
 import ParticlesBackground from './ParticlesBackground';
 import Master from '../assets/Master.png'
+import { motion } from 'framer-motion'
 
 const MasterArtGaming = () => {
     return (
@@ -19,7 +20,7 @@ const MasterArtGaming = () => {
                     alt=""
                 />
                 {/* Text Section */}
-                <div className='flex flex-col items-center gap-10 text-[#d1d5db] '>
+                <motion.div className='flex flex-col items-center gap-10 text-[#d1d5db] '>
                     <h1 className='text-3xl font-extrabold '>
                         Elevate Your Gaming Supremacy
                     </h1>
@@ -31,8 +32,15 @@ const MasterArtGaming = () => {
                         Immerse yourself in the ultimate gaming adventure with our curated selection of
                         premium equipment—from the newest consoles and controllers to high-performance gear.
                     </p>
-
-                </div>
+                    <motion.button
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                        className='bg-gradient-primary duration-500 w-[150px] p-3 
+                        tracking-widest rounded-full font-semibold shadow-lg hover:shadow-xl opacity-'
+                    >
+                        Buy Now
+                    </motion.button>
+                </motion.div>
             </div>
         </div>
     )
